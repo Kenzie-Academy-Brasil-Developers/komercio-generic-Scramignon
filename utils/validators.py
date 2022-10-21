9d1fcd0b259b3a33dfc93efe6119fe99477b5210
Fernando Scramignon <scramignonnarde@gmail.com>
2022-10-21 14:10:31 -0300

    feat: creates fixture

--- utils/validators.py
+++ utils/validators.py
@@ -5,6 +5,4 @@ def validate_2_decimal_places(value):
     try:
         return round(float(value), 2)
     except:
-        raise ValidationError(
-            f"{value} is in a wrong format"
-        )
+        raise ValidationError(f"{value} is in a wrong format")
